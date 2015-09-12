d4c8f6f31c8bcf3706030d6e29e1d62c57431513
Brad Merrill <brad_merrill@hotmail.com>
2015-09-12 13:31:02 -0700

    add console driver

---
+++ CsLexConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LexLib;
+
+namespace CsLexConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gen lg;
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("lex <filename>");
+                return;
+            }
+
+            lg = new Gen(args[1]);
+            lg.generate();
+        }
+    }
+}
